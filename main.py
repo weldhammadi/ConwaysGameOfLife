0f0738a69c7fb11f1b8b2348e05076a534afa9ef
hamza elmortada <hamza.el-mortada@edu.isga.ma>
2024-12-18 15:50:16 +0100

    ajout de main.py remplie par la structure du jeu

---
+++ main.py
@@ -0,0 +1,44 @@
+import numpy
+
+frame = numpy.array([[0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 1, 1, 1, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0]])
+
+def compute_number_neighbors(paded_frame, index_line, index_column):
+    number_neighbors=0
+    """
+    Cette fonction prend en entrée la matrice avec bordure et
+    renvoie le nombre de cellules voisines vivantes.
+    """
+    return number_neighbors
+
+def compute_next_frame(frame):
+    """
+    Cette fonction prend en entrée une frame et calcule la frame suivante
+    à partir des règles du jeu de la vie
+    """
+    paded_frame = numpy.pad(frame, 1, mode="constant") # je vous offre le code pour le zéro padding c'est cadeau !
+    """
+    # Étape 1 : 2 boucles for imbriquées pour parcourir la matrice avec bordure (zero padding) element par element.
+    Faites attention à l'index de début et d'arrêt ! (il s'agit de la matrice avec bordure)
+
+    # L'étape 2 et 3 se font au cours de la même itération (attention à l'indentation !)
+    
+    # Étape 2 : Pour chacun des éléments, calculez le nombre de voisins.
+    On fait appelle à la fonction (compute_number_neighbors)
+    
+    # Étape 3 : Pour chacun des éléments faire les tests (état de l'élément et son nombre de voisin) afin de voir
+    si il y a des modifications à faire.
+    Si c'est le cas effectuez les modifications directement dans la matrices frame (Attention à l'indice
+    utilisé ! )"""
+    
+    return frame
+
+while True:
+    # boucle infini qui affiche toutes les frames successives (ctrl + c pour arrêter le script)
+    print(frame)
+    frame = compute_next_frame(frame)
